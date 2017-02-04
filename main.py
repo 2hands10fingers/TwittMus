145346e4a55c1c9edf1eb3efbb9e60a90fe4c302
2hands10fingers <antonio.nogueras@yahoo.com>
2017-02-04 16:53:29 -0600

    Update main.py

--- main.py
+++ main.py
@@ -3,10 +3,10 @@ from tweepy import OAuthHandler
 from tweepy.streaming import StreamListener
 import time
 
-ckey = 'Xwk45yibY06n7wHFHuoI2KVhi'
-csecret = 'uWbOJZdRTmLDFKIxJKoR3huDwprOqefpwctEMIfE3gXfwruN5H'
-atoken = '3096677539-t2d26ZLh1YbciYRub7TtGfEvrhF5MxhRTsNjuIb'
-asecret = 'N4sSNMBw0TLG8yEwcGRAL15bSSeK1oO8SBZG63B8WiE9s'
+ckey = 'Cr6ZcBdW4MC8yo6foRAMPp09h'
+csecret = 'cQizXmeD4wv4A5Ek2F8LNQ2sQoVfYmDJhRTEqBKD7EdNTKj7b2'
+atoken = '825875016608841730-TKbKCdp0Z55L99nLvw9gRjfz33nP8GG'
+asecret = '9I5EeD1v8Dw9lxbwpuf1FI2wpRIQospygJD3A3Z0bNKAY'
 
 """ MIDI NOTES ASSIGN nos harps or flats
 
